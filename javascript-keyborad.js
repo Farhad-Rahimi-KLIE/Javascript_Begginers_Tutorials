73b524b4bd7cb5071ce29decf27fc6180ab38289
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-06 14:55:48 +0430

    Javascript-keyword was uplaoded

---
+++ javascript-keyborad.js
@@ -0,0 +1,18 @@
+/*
+ . javascript have a reserved keywords
+ 1 - break;
+ 2 - continue;
+ 3 - if
+ 4 - else
+ 5 - debugger
+ 6 - do...while
+ 7 - for
+ 8 - function
+ 9 - return
+ 10- switch
+ 11 - try
+ 12 - catch
+ 13 - var
+ 14 - let
+ 15 - const
+*/
