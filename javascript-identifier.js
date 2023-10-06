8cb0620ff105daa68fd6b130f0a8ccde5a3af169
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-06 14:57:31 +0430

    Javascript-identifier was uplaoded

---
+++ javascript-identifier.js
@@ -0,0 +1,7 @@
+// All javascript variables must be identified with unique names
+/*
+ 1 - Names can contain letters,digits,underscores,and dollar sign;
+ 2 - Names must begin with a letter
+ 3 - Names can also begin with $,and _
+ 4 - Names are case Sensitive(y or Y different value);
+*/
