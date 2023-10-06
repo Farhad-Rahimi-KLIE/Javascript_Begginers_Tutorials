59448d4d0246c28d7d8970b257c1a3bcaa1686e8
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-06 14:42:39 +0430

    Why-stady-javascript was uplaoded

---
+++ why_stady-js.js
@@ -0,0 +1,7 @@
+/*
+ javascript is one of the 3 languages all
+ web developers must learn:
+ 1 - HTML to define the content of web pages
+ 2 - CSS to specify the layout of web pages
+ 3 - javascript to programme the behavior of web pages
+*/
