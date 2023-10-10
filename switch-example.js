62e5cc47c63de188500558c1756e67270d295dc7
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-10 05:19:25 +0430

    Add files via upload

---
+++ switch-example.js
@@ -0,0 +1,17 @@
+switch(new Date().getday()){
+ case 0:
+ day = "monday";
+ break;
+ case 1:
+ day = "tuesday";
+ break;
+ case 2:
+ day = "wednedday";
+ break;
+ case 3:
+ day = "thursday";
+ break;
+ case 4:
+ day = "friday";
+ break;
+}
