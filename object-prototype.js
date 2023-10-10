f3b85b2402fbd0b27dc1ec039b937db44fc111eb
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-10 15:37:39 +0430

    Add files via upload

---
+++ object-prototype.js
@@ -0,0 +1,9 @@
+function person(first,last,age,eyecolor){
+ this.firstname = first;
+ this.lastname = last;
+ this.age = age;
+ this.eteColor = etecolor;
+}
+
+var myFather = new person("farhad","rahini",23,"brown")
+var myMother = new person("Shafiqa","rahim",23,"green")
