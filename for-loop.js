1796cbfed3d865492f9bd522900a6873850dacf6
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-10 05:03:05 +0430

    For-loop was uplaoded

---
+++ for-loop.js
@@ -0,0 +1,11 @@
+text+= cars[0] + "<br>";
+text+= cars[1] + "<br>";
+text+= cars[2] + "<br>";
+text+= cars[3] + "<br>";
+text+= cars[4] + "<br>";
+text+= cars[5] + "<br>";
+
+var i;
+for(var i=0; i<cars.length: i++){
+ text+=cars[i] + "<br>"
+}
