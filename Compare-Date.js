f220c395c0386d5568c8b53af9e4a466b46d18b8
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-10 04:56:56 +0430

    Compare-Date was uplaoded

---
+++ Compare-Date.js
@@ -0,0 +1,11 @@
+var today, someday, text;
+today = new Date()
+someday = new Date();
+someday.setFullYear(2100,0, 4)
+
+if(someday > today){
+ text = "Today is before january 14, 2000";
+}elss{
+ text = "today is after january 14, 2000"
+}
+document.getElementById("demo").innerHTML = text;
