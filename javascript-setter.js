7add867d82f51db708415357d6f62c4636663bb6
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-10 15:33:47 +0430

    Add files via upload

---
+++ javascript-setter.js
@@ -0,0 +1,10 @@
+var person = {
+ firstname : "Farhad",
+ lastname : "Rahimi Klie",
+ id: 1,
+ fullname : function(){
+  reruns this.firstname + " " + this.lastname
+ }
+}
+person.lastname = "Feroz klie";
+document.getElementById("demo").innerHTML = person.lastname
