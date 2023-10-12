bf4ef72aadc4d184e75a91c9912e479ed3758d72
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-13 04:00:43 +0430

    Add files via upload

---
+++ Finding-html-element.js
@@ -0,0 +1,5 @@
+1 - finding html elements by id
+2 - finding html elements by tag name
+3 - finding html elements by class name
+4 - finding html elements by CSS Selectar
+5 - finding html elements by html object collections
