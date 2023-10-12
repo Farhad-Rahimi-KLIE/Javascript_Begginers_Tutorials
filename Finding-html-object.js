ee0bb748aaaa657dc94142b5d5a8b496c75d40cc
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-13 04:00:20 +0430

    Add files via upload

---
+++ Finding-html-object.js
@@ -0,0 +1,12 @@
+/*
+ 1 - document.anchors
+ 2 - document.applets
+ 3 - document.baseURL
+ 4 - document.body
+ 5 - document.html
+ 6 - document.link
+ 7 - document.images
+ 8 - document.head
+ 9 - document.title
+ 10 - document.all
+*/
