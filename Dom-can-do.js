aa48ee2cf697e9cf4e7c42d6cb82ab03ec380b99
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-13 04:04:15 +0430

    Add files via upload

---
+++ Dom-can-do.js
@@ -0,0 +1,5 @@
+/*
+ 1 - javascript can change all the html element in the page
+ 2 - javascript can change all the html Attribute in the page
+ 3 - javascript can change all the css style in the page
+*/ 
