84fe7787f617034898a7005cf0f4e0fc5faf9254
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-13 04:23:28 +0430

    Update DOM Node.js

--- DOM Node.js
+++ DOM Node.js
@@ -1,5 +1,5 @@
 /*
- 1 - the entire document is a document node
+ 1 - the entire document is a document nodes
  2 - every html element is an element node
  3 - the text inside html element are text node
  4 - All comments are comment node
