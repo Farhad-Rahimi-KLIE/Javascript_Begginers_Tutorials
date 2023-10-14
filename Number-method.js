27ef87ef8a7257a0d05a72a66d1d7ef97b270254
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-14 06:25:55 +0430

    Update Number-method.js

--- Number-method.js
+++ Number-method.js
@@ -1,4 +1,4 @@
-//javascript Number Methods
+//Javascript Number Methods
 //Number method and property are also available to primitive value,
 //because javascript treats primitive values as object
 //when executing methods and property.
