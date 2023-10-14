743a279bcc9cd5fa880cbee5c37e0cf6f11dfb06
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-14 05:22:25 +0430

    Add files via upload

---
+++ Ajax-function.js
@@ -0,0 +1,10 @@
+function loadDic(){
+ var xhttp = new XMLHttpRequest();
+ xhttp.onreadystatechange = function(){
+  if(this.readyState==4 && this.status ==200){
+   document.getElementById("demo").innerHTML = this.responseText;
+  }
+ }
+ xhttp.open("GET","ajax.info.txt",true);
+ xhttp.dend()
+}
