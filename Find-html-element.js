8eae329b0ab1f5d739a4afdab2e2682c8e6855f7
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-14 06:08:38 +0430

    Update Find-html-element.js

--- Find-html-element.js
+++ Find-html-element.js
@@ -1,5 +1,5 @@
 /* 
-finger javascript element 
+finger Javascript element 
 1 - document.getElementById("demo")
 2 - document.getElementByClassName("demo")
 3 - document.getElementByTagName("demo")
