9075c75e12b49990f210b3dde41c33e93ed844f4
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-14 04:59:46 +0430

    Add files via upload

---
+++ Storing-Data.js
@@ -0,0 +1,10 @@
+// Storing Data
+var myjson = '{name : "farhad", fname:"Rahimi"}';
+var myobj = JSON.stringify(myjson);
+LocalStorage.setItem("testJSON",myobj);
+
+
+// Retrieving Data
+text = LocalStorage.setItem("textJSON");
+obj = JSON.parse(text)
+document.getElementById("demo").innerHTML = obj.name
